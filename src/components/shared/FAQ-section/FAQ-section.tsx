7602ee9169f7076b31d1e182776c9291a2a8945e
Sula <turatbekovchannel2018@gmail.com>
2024-03-16 21:07:36 +0600

    fix - bags

--- src/components/shared/FAQ-section/FAQ-section.tsx
+++ src/components/shared/FAQ-section/FAQ-section.tsx
@@ -2,7 +2,7 @@ import {FaqAccordion} from "@/src/components/shared/FAQ-section/FaqAccordion/Faq
 import {FaqForm} from "@/src/components/shared/FAQ-section/FaqForm/FaqForm";
 const FAQSection = () => {
     return (
-        <section className={`flex flex-col items-center justify-center gap-10 lg:flex-row lg:justify-between py-[120px]`}>
+        <section className={`flex flex-col items-center justify-center gap-10 py-[120px] lg:flex-row lg:justify-between`}>
             <FaqAccordion/>
             <FaqForm/>
         </section>
