c9c322860ad30817d578a60538ee8edaf9f876d6
Sula <turatbekovchannel2018@gmail.com>
2024-04-18 15:49:35 +0600

    fix and make: fixed bugs full screen, connect to backEdn

--- src/components/shared/SpecialDeals/SpecialDeals.tsx
+++ src/components/shared/SpecialDeals/SpecialDeals.tsx
@@ -31,7 +31,7 @@ export function SpecialDeals() {
     }, [locale]);
     return (
 
-        <section className="flex size-full max-h-[750px] flex-col items-center justify-center rounded antialiased">
+        <section className="flex size-full h-[350px] sm:h-[550px] lg:h-[700px] flex-col items-center justify-start rounded antialiased">
             <div className="mt-5 flex w-full flex-col items-start justify-between sm:mt-10 sm:flex-row sm:items-center md:mt-16 lg:mt-24">
                 <h3 className="text-base font-bold md:text-lg lg:text-xl">{t('title')}</h3>
                 <Link href={LinkEnum.Flights}>
@@ -43,7 +43,7 @@ export function SpecialDeals() {
                     </Button>
                 </Link>
             </div>
-            <div className={`relative mb-5 h-[600px] w-full sm:mb-10 sm:mt-2 md:mb-16 md:mt-5 lg:mb-24 lg:mt-10`}>
+            <div className={`absolute top-[5750px] sm:top-[5300px] md:top-[3500px] lg:top-[2600px] mb-5 max-h-[550px] size-full sm:mb-10 sm:mt-2 md:mb-16 md:mt-5 lg:mb-24 lg:mt-10`}>
                 {
                     isLoading ? <div className={`w-full py-20 text-center text-lg md:text-2xl`}>{t('loading')}</div> :
                         data ? <InfiniteMovingCards
--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -49,11 +49,11 @@ export const InfiniteMovingCards: React.FC<ISpecialDealsProps> = ({
                 )}
             >
                 {items.map((element, elementId) => (
-                    <div key={elementId} className="relative flex flex-col gap-1 rounded border p-1 sm:gap-2 sm:rounded-sm sm:p-2 md:gap-3 md:rounded-xl md:p-3 lg:gap-4 lg:p-4">
-                        <div className={`max-w-[522px] max-h-[327px]`}>
+                    <div key={elementId} className="relative max-w-[200px] sm:max-w-[300px] md:max-w-[400px] lg:max-w-[575px] flex flex-col gap-1 rounded border p-1 sm:gap-2 sm:rounded-sm sm:p-2 md:gap-3 md:rounded-xl md:p-3 lg:gap-4 lg:p-4">
+                        <div className={`max-h-[100px] sm:max-h-[200px] md:max-h-[250px] lg:max-h-[327px]`}>
                             <img className={`size-full`} src={element.imageUrl} alt={element.flight.to}/>
                         </div>
-                        <h4 className="max-w-[180px] text-[12px] sm:text-xs md:max-w-[525px] md:text-base">{element.description}</h4>
+                        <h4 className="max-w-[180px] sm:max-w-[300px] text-[12px] sm:text-xs md:max-w-[525px] md:text-base truncate ">{element.description}</h4>
                         <div className="flex flex-col gap-0 md:gap-2 mt-auto">
                             <p className="text-[10px] text-red-700 sm:text-caption md:text-xs lg:text-sm">{t('discount')}{element.discount}%</p>
                             <Link href={LinkEnum.Flights}>
