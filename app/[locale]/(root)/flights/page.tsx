9ba0de2a248fbce5fe58a49c0f6994aa1247cdad
Dastan <xypxzy@gmail.com>
2024-04-24 14:50:46 +0600

    fix: flights page - console log errors

--- app/[locale]/(root)/flights/page.tsx
+++ app/[locale]/(root)/flights/page.tsx
@@ -1,24 +1,20 @@
 import Hero from '@/src/components/common/Hero/Hero'
-import {Filter} from "@/src/components/shared/FilterComponents/Filter";
-import {BestPrices} from "@/src/components/shared/FlightsComponents/BestPrices/BestPrices";
-
+import { Filter } from '@/src/components/shared/FilterComponents/Filter'
+import { BestPrices } from '@/src/components/shared/FlightsComponents/BestPrices/BestPrices'
+import { Skeleton } from '@/src/components/ui/skeleton'
 
 export default function Flights() {
 	return (
 		<div>
 			<Hero title='Discover Flights' type='flights' />
-			<div className={`mt-5 flex flex-wrap-reverse lg:flex-nowrap`}>
-				<div className={`hidden w-full max-w-[390px] md:block`}>
-					<Filter/>
+			<div className={`my-16 flex flex-wrap-reverse gap-6 lg:flex-nowrap`}>
+				<div className={`hidden w-3/12 md:block`}>
+					<Filter />
 				</div>
-				<div className={`w-full max-w-[886px]`}>
-					<BestPrices/>
+				<div className={'flex-1'}>
+					<BestPrices />
 				</div>
-				<div className={`mb-5 h-[200px] w-full px-1 lg:mb-0 lg:max-h-screen lg:max-w-[310px]`}>
-					<div className={`size-full rounded-xl bg-slate-500`}>
-						reclama
-					</div>
-				</div>
+				<Skeleton className='h-[450px] w-2/12 rounded-xl bg-slate-400' />
 			</div>
 		</div>
 	)
---
+++ src/components/ui/skeleton.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/src/shared/utils/classnames'
+import React from 'react'
+
+function Skeleton({
+	className,
+	...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+	return (
+		<div
+			className={cn('animate-pulse rounded-md bg-muted', className)}
+			{...props}
+		/>
+	)
+}
+
+export { Skeleton }
