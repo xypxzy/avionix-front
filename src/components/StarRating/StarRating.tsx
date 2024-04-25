61e8b1a0ece6f506541ca4acc789d5852e61cb8b
Sula <turatbekovchannel2018@gmail.com>
2024-04-25 14:28:22 +0600

    fix: bugs comments

--- src/components/StarRating/StarRating.tsx
+++ src/components/StarRating/StarRating.tsx
@@ -1,11 +1,14 @@
-import {StarIcon} from "@heroicons/react/16/solid";
 
 export const StarRating = ({rating}:{rating:number}) => {
 
     return (
-        <div className="flex items-center justify-end w-1/2 h-full">
+        <div className="flex h-full w-1/2 items-center justify-end">
             {[...Array(5)].map((_, index) => (
-                <StarIcon key={index} className={`size-10 ${rating>Math.min(index, rating) ? 'text-gold' : ''}`}/>
+                <svg key={index} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill={`${rating>Math.min(rating, index) ? 'gold': 'none'}`}
+                     stroke={`${rating>Math.min(rating, index) ? 'gold': '#F4F1EC'}`}>
+                    <polygon
+                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
+                </svg>
             ))}
         </div>
     );
--- src/components/shared/Comments/CommentsCarousel/CarouselItems.tsx
+++ src/components/shared/Comments/CommentsCarousel/CarouselItems.tsx
@@ -1,13 +1,18 @@
 import {IComments} from "@/src/shared/types/commentsTypes";
 import {StarRating} from "@/src/components/StarRating/StarRating";
+import { format, Locale } from 'date-fns';
+import {useLocale} from "next-intl";
+import { enGB, tr, ru } from "date-fns/locale";
 
 export const CarouselItems = ({item}:{item:IComments}) => {
+    const locale = useLocale()
+    const lan: Locale | undefined = locale === 'ru' || locale === 'ky' ? ru : locale ==='en' ? enGB : locale === 'tr' ? tr : null
     return (
-        <div className="p-3 flex flex-col bg-dark_blue rounded-xl text-background_hero gap-4">
+        <div className="flex flex-col gap-4 rounded-xl bg-dark_blue p-3 text-background_hero">
             <h4 className={`text-end`}>{item.author}</h4>
             <p>{item.description}</p>
-            <div className={`flex justify-between mt-auto items-center`}>
-                <p>{item.createdAt}</p>
+            <div className={`mt-auto flex items-center justify-between`}>
+                <p>{item.createdAt && locale && format(new Date(item.createdAt), 'dd MMMM yyyy', { locale: lan})}</p>
                 <StarRating rating={item.grade}/>
             </div>
         </div>
