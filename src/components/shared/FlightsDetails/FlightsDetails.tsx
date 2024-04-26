ed69d1c9a1ae2750c7f6b8a59fe78f4bc3a7c257
Dastan <xypxzy@gmail.com>
2024-04-26 08:36:41 +0600

    add stripe

--- src/components/shared/FlightsDetails/FlightsDetails.tsx
+++ src/components/shared/FlightsDetails/FlightsDetails.tsx
@@ -3,10 +3,13 @@
 import FlightService from '@/src/services/api/flight-host'
 import { IFlight } from '@/src/shared/types/topFlightsTypes'
 import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import { format } from 'date-fns'
 import { CalendarDays } from 'lucide-react'
 import { useLocale } from 'next-intl'
 import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '../../ui/button'
 import { Separator } from '../../ui/separator'
 import { Skeleton } from '../../ui/skeleton'
 
@@ -18,6 +21,23 @@ export function FlightsDetails({ id }: { id: string }) {
 			FlightService.getFlight(id, {
 				lan: locale,
 			}),
+	})
+
+	const { data: paymentLink } = useQuery<{
+		paymentLinkId: string
+		paymentLink: string
+	}>({
+		queryKey: ['flight-link'],
+		queryFn: async () => {
+			try {
+				const response = await axios.get(
+					`http://35.232.168.91:8072/avionix/flight/api/ticket/paymentLink?flightId=${flight?.id}&checkedBaggageIncluded=true`
+				)
+				return response.data
+			} catch (error) {
+				throw new Error(`Failed to fetch flight details: ${error}`)
+			}
+		},
 	})
 
 	if (isLoading || !flight) {
@@ -143,16 +163,23 @@ export function FlightsDetails({ id }: { id: string }) {
 					</div>
 				</div>
 			</div>
-			<div className='h-fit w-2/5 rounded-md border-2'>
-				<div className='flex flex-1 gap-4 rounded-t-sm bg-primary p-4 px-8 text-base font-normal text-primary-foreground transition-all sm:items-center'>
-					Price Details
+			<div className='h-fit w-2/5'>
+				<div className='rounded-md border-2'>
+					<div className='flex flex-1 gap-4 rounded-t-sm bg-primary p-4 px-8 text-base font-normal text-primary-foreground transition-all sm:items-center'>
+						Price Details
+					</div>
+					<div className='flex items-center justify-between p-4 px-8'>
+						<span>1 x Passenger</span>
+						<span>
+							{flight.currency} {flight.tariff.price}
+						</span>
+					</div>
 				</div>
-				<div className='flex items-center justify-between p-4 px-8'>
-					<span>1 x Passenger</span>
-					<span>
-						{flight.currency} {flight.tariff.price}
-					</span>
-				</div>
+				<Link href={paymentLink?.paymentLink || '#'}>
+					<Button variant='default' className='mt-5 w-full'>
+						Купить
+					</Button>
+				</Link>
 			</div>
 		</div>
 	)
