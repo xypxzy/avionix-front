b552f4b087972c1b06bb1c96b773720341f95320
Sula <turatbekovchannel2018@gmail.com>
2024-04-29 23:08:18 +0600

    fix: globalization breadCamp and Trigger

--- app/[locale]/(root)/profile/page.tsx
+++ app/[locale]/(root)/profile/page.tsx
@@ -15,8 +15,10 @@ import {useEffect} from "react";
 import {GeneralInfo} from "@/src/components/shared/UserAccount/GeneralInfo/GeneralInfo";
 import {Booking} from "@/src/components/shared/UserAccount/Booking/Booking";
 import {FlightHistory} from "@/src/components/shared/UserAccount/FlightHistory/FlightHistory";
+import {useTranslations} from "next-intl";
 export default function Profile() {
   const { fetchUser, isLoading, user} = useUserStore()
+  const t = useTranslations('userProfile')
 
   useEffect(() => {
     fetchUser()
@@ -33,23 +35,23 @@ export default function Profile() {
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem>
-                  <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                  <BreadcrumbLink href="/">{t('breadCamp.home')}</BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator>
                   <Slash/>
                 </BreadcrumbSeparator>
                 <BreadcrumbItem>
-                  <BreadcrumbPage>Account</BreadcrumbPage>
+                  <BreadcrumbPage>{t('breadCamp.account')}</BreadcrumbPage>
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
           </div>
           <div className={`w-full`}>
             <Tabs defaultValue="g-info" className="w-full">
-              <TabsList className="grid w-[450px] grid-cols-3">
-                <TabsTrigger value="g-info">General info</TabsTrigger>
-                <TabsTrigger value="booking">Booking</TabsTrigger>
-                <TabsTrigger value="flights">Flight history</TabsTrigger>
+              <TabsList className="grid max-w-[500px] grid-cols-3">
+                <TabsTrigger value="g-info">{t('nav.generalInfo')}</TabsTrigger>
+                <TabsTrigger value="booking">{t('nav.booking')}</TabsTrigger>
+                <TabsTrigger value="flights">{t('nav.flightHistory')}</TabsTrigger>
               </TabsList>
               <TabsContent value="g-info" className={`w-full`}>
                 <GeneralInfo/>
