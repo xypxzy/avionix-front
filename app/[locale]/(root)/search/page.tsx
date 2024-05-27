b5a0b4b9144e273631b9b65bdade56c99a064bf4
Sultan <turatbekovchannel2018@gmail.com>
2024-05-27 17:26:51 +0600

    backup

--- app/[locale]/(root)/search/page.tsx
+++ app/[locale]/(root)/search/page.tsx
@@ -1,19 +1,19 @@
 'use client'
 
-import { FaqAccordion } from '@/src/components/shared/FAQ-section/FaqAccordion/FaqAccrodion'
 import { useSearchStore } from '@/src/stores/search.store'
+import {useEffect} from "react";
 
 export default function SearchPage() {
 	const { searchResult } = useSearchStore()
 
-	console.log(searchResult)
+	useEffect(() => {
+		console.log(searchResult)
+	}, []);
 
 	return (
-		searchResult?.faqs && (
+		searchResult?.flights && (
 			<div>
-				{searchResult?.faqs.length > 0 && (
-					<FaqAccordion faqList={searchResult?.faqs} />
-				)}
+			 search
 			</div>
 		)
 	)
